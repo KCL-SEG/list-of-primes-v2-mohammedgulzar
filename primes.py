4e449a97bcce195b6529de0cdf146928705c1b53
Mohammadgulzar Habibullah <mohammedgulzar79@gmail.com>
2023-09-24 15:28:25 +0100

    Completed Primes exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        primeFlag = True
+        for i in range(2, num // 2 + 1):
+            if num % i == 0:
+                primeFlag = False
+        if primeFlag:
+            list.append(num)
+        num += 1
     return list
